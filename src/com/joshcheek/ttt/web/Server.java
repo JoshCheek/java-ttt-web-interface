351fc1f3a7cf39e83ee02ec5c2d87c8a7528d09b
Josh Cheek <josh.cheek@gmail.com>
2011-09-07 11:23:59 -0500

    initial commit

---
+++ src/com/joshcheek/ttt/web/Server.java
@@ -0,0 +1,12 @@
+package com.joshcheek.ttt.web;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: joshuajcheek
+ * Date: 9/7/11
+ * Time: 11:06 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Server {
+
+}
---
+++ test/com/joshcheek/ttt/web/ServerTest.java
@@ -0,0 +1,26 @@
+package com.joshcheek.ttt.web;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: joshuajcheek
+ * Date: 9/7/11
+ * Time: 11:07 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class ServerTest extends junit.framework.TestCase {
+    // as a player
+        // given i go to the homepage then i should be prompted to go first or second
+
+        // given i have been prompted to go first or second when i click first then i should be presented with an empty board
+
+        // given i have been prompted to go first or second when i click second i should be presented with a board that has 1 x on it
+
+        // given i have a board with one x in the upper left corner, when i render the board i should see a game with no link in the upper left corner
+
+        // given i have a board with nothing clicked and i click a square, it should be filled in and so should the computer's move
+
+        // given the computer wins the game then the game should be over
+
+        // given the game is over then there should be nowhere for me to move and there should be a link to play again and when i click it i should be taken to the homepage
+
+}
